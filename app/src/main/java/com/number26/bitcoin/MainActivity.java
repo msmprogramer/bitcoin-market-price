1680e1ca04aed1043500b33906a2c5bc2079065e
msalama <msalama@apptitude-eg.com>
2016-03-21 22:45:14 +0200

    init Bitcoin app

---
+++ app/src/main/java/com/number26/bitcoin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.number26.bitcoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
